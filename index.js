e39e0152ffa973b455916818ea4613e5228cffdf
JoeySniper <89961122+JoeySniper@users.noreply.github.com>
2022-02-04 20:18:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/JoeySniper/JoeySniper.git
+git remote add origin https://github.com/JoeySniper/myapp.git
 git push -u origin main
